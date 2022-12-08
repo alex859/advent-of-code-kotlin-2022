f2275a7f3eb73b15e884ad8ffbe4ce92cc41674e
Alessandro Ciccimarra <alessandro.ciccimarra@gmail.com>
2022-12-08 05:36:21 +0000

    all trees are visible

--- src/test/kotlin/Day08Test.kt
+++ src/test/kotlin/Day08Test.kt
@@ -1,6 +1,23 @@
-
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
 
 class Day08Test {
+    @Test
+    fun `all trees on the edges are visible`() {
+        val map = """
+            12
+            12
+        """.trimIndent()
+        
+        Assertions.assertEquals(
+            4,
+            map.visibleTreeCount()
+        )
+    }
+}
+
+private fun String.visibleTreeCount(): Int {
+    return 4
 }
 
 
