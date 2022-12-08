fa82ffdb4c717c9070634279e8d8b62cf655cb81
Alessandro Ciccimarra <alessandro.ciccimarra@gmail.com>
2022-12-08 05:41:51 +0000

    add test for middle tree visible

--- src/test/kotlin/Day08Test.kt
+++ src/test/kotlin/Day08Test.kt
@@ -26,6 +26,47 @@ class Day08Test {
             map.visibleTreeCount()
         )
     }
+
+    @Test
+    fun `all trees are visible - also inside`() {
+        val map = """
+            132
+            132
+        """.trimIndent()
+
+        Assertions.assertEquals(
+            6,
+            map.visibleTreeCount()
+        )
+    }
+
+    @Test
+    fun `all trees are visible - more trees`() {
+        val map = """
+            132
+            357
+            132
+        """.trimIndent()
+
+        Assertions.assertEquals(
+            9,
+            map.visibleTreeCount()
+        )
+    }
+
+    @Test
+    fun `tree in the middle is not visible`() {
+        val map = """
+            132
+            307
+            132
+        """.trimIndent()
+
+        Assertions.assertEquals(
+            8,
+            map.visibleTreeCount()
+        )
+    }
 }
 
 private fun String.visibleTreeCount(): Int {
